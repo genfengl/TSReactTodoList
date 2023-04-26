91e3d437a6ae4c109b708b0d2f54f326b469e256
genfengl <genfeng.liu@gmail.com>
2023-04-26 19:22:52 +1000

    added newTodo and todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import React from 'react'
+import TodoList from './components/TodoList'
+import NewTodo from './components/NewTodo'
 
-import './App.css'
+const App: React.FC = () => {
+  const todos = [{ id: 't1', text: 'Finish the course' }]
 
-function App() {
- 
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  }
 
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      <NewTodo todoAddHandler={todoAddHandler}/>
+      <TodoList todos={todos}/>
+    </div>
   )
 }
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 
-const TodoList: React.FC = () => {
-  const todos = [{ id: 't1', text: 'Finish the course' }]
+interface TodoListProps {
+  todos: { id: string, text: string }[]
+}
+
+const TodoList: React.FC<TodoListProps> = ({ todos }) => {
   return (
     <ul>
-      {todos.map((todo) => {
-        return (
-          <li>
-
-          </li>
-        )
-      })}
+      {todos.map(todo =>
+      (
+        <li key={todo.id}>
+          {todo.text}
+        </li>
+      )
+      )}
     </ul>
   )
 }
