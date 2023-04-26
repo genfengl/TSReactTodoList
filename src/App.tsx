b6392e2b54eae876908dd7b3954fe3694d4a328d
genfengl <genfeng.liu@gmail.com>
2023-04-26 23:35:22 +1000

    todoList done

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import TodoList from './components/TodoList'
-import NewTodo from './components/NewTodo'
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
 
 const App: React.FC = () => {
-  const todos = [{ id: 't1', text: 'Finish the course' }]
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId)
+    })
   }
 
   return (
-    <div className='App'>
-      <NewTodo todoAddHandler={todoAddHandler}/>
-      <TodoList todos={todos}/>
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList todos={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
+import React from "react";
+
+import './TodoList.css'
 
 interface TodoListProps {
-  todos: { id: string, text: string }[]
+  todos: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
 }
 
-const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+const TodoList: React.FC<TodoListProps> = ({ todos, onDeleteTodo }) => {
   return (
     <ul>
-      {todos.map(todo =>
-      (
+      {todos.map((todo) => (
         <li key={todo.id}>
-          {todo.text}
+          <span>{todo.text}</span>
+          <button onClick={() => onDeleteTodo(todo.id)}>DELETE</button>
         </li>
-      )
-      )}
+      ))}
     </ul>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
